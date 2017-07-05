d7e8b0a001749173c97eb4a917b58330629c42ad
Daniel Kling <danielklingjr@gmail.com>
2017-07-04 21:42:29 -0400

    initial commit

--- index.php
+++ index.php
@@ -1,4 +1,8 @@
-<?php include("includes/header.php") ?>
+<?php
+$page_title = "Home Page";
+$page_description = "This is the homepage for a thing I'm working on.";
+$page_keywords = "web,design,fun";
+include("includes/header.php") ?>
 
 
 
